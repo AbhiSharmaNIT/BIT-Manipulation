349cc2edf708707ace5d1dcdc24f6154b0ef104c
Abhishek Kumar <136017324+AbhiSharmaNIT@users.noreply.github.com>
2024-01-15 22:43:01 +0530

    Add files via upload

---
+++ problem3.cpp
@@ -0,0 +1,35 @@
+//Ques:- Find the position of the rightmost set bit
+
+#include <iostream>
+using namespace std;
+ 
+// Returns the position of the rightmost set bit of `n`
+int positionOfRightmostSetBit(int n)
+{
+    // if the number is odd, return 1
+    if (n & 1) {
+        return 1;
+    }
+ 
+    // unset rightmost bit and xor with the number itself
+    n = n ^ (n & (n - 1));
+ 
+    // find the position of the only set bit in the result;
+    // we can directly return `log2(n) + 1` from the function
+    int pos = 0;
+    while (n)
+    {
+        n = n >> 1;
+        pos++;
+    }
+    return pos;
+}
+ 
+int main()
+{
+    int n = 20;
+
+    cout << "The position of the rightmost set bit is "<< positionOfRightmostSetBit(n);
+ 
+    return 0;
+}
